70eab5586ed304d158aaa62e9132fa90cc1fc873
Lucas Sousa Meireles <meireles31@msn.com>
2016-08-28 15:50:20 +0100

    Now prints sorted array based on movie length

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from config import *
 from FilmowParser import FilmowParser
 
+def movieCmp(movie1, movie2):
+    duration1 = int(movie1['duration'].split(' ')[0])
+    duration2 = int(movie2['duration'].split(' ')[0])
+    if duration1 < duration2:
+        return -1
+    elif duration1 > duration2:
+        return 1
+    else:
+        return 0
+
 def main():
     print config
     filmowparser = FilmowParser(config['filmowURL'], config['filmowUsername'])
-    print filmowparser.getWantToSeeMovies()
+    moviesVec = filmowparser.getWantToSeeMovies()
+    moviesVec.sort(cmp=movieCmp)
+    print moviesVec
 
 if __name__ == '__main__':
     main()
