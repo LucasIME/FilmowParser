7446166a3730622a480b832e27812d3e4ee6e675
Lucas Meireles <meireles31@msn.com>
2017-06-20 19:48:08 -0300

    Implemented function to get want to see pages correctly

--- FilmowParser.py
+++ FilmowParser.py
@@ -11,17 +11,27 @@ class FilmowParser():
     def __init__(self, baseURL, username):
         self.baseURL = baseURL
         self.username = username
+        hdr = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+            'Accept-Encoding': 'none',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Connection': 'keep-alive'}
 
     def getWantToSeePages(self):
-        return 6
+        want_to_see_url = self.baseURL + '/usuario/' + self.username + '/quero-ver/';
+        wantToSeeCatalogueHTML = urllib.request.urlopen(urllib.request.Request(want_to_see_url, headers=self.hdr))
+        catalogueSoup = BeautifulSoup(wantToSeeCatalogueHTML, 'html.parser')
+        page_list = catalogueSoup.find('div', class_="pagination pagination-centered").find_all('a')
+        last_page_index = 1
+        for page in page_list:
+            page_index = int(page['href'].split('=')[-1])
+            last_page_index = max(page_index, last_page_index)
+        return last_page_index
 
     def getWantToSeeMovies(self):
-        hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-       'Accept-Encoding': 'none',
-       'Accept-Language': 'en-US,en;q=0.8',
-       'Connection': 'keep-alive'}
+
         searchURL = self.baseURL + '/usuario/' + self.username + '/quero-ver/';
 
         moviesVec = []
@@ -40,7 +50,7 @@ class FilmowParser():
         threadPool.startWorking()
 
         def parsePage(pageUrl):
-            wantToSeeCatalogueHTML = urllib.request.urlopen(urllib.request.Request(pageUrl, headers=hdr))
+            wantToSeeCatalogueHTML = urllib.request.urlopen(urllib.request.Request(pageUrl, headers=self.hdr))
             catalogueSoup = BeautifulSoup(wantToSeeCatalogueHTML, 'html.parser')
             print(pageUrl)
             #looping through each movie in the current page
@@ -53,7 +63,7 @@ class FilmowParser():
         
         def parseMovie(movieURL):
             movie = {}
-            moviePageHtml = urllib.request.urlopen(urllib.request.Request(movieURL, headers=hdr))
+            moviePageHtml = urllib.request.urlopen(urllib.request.Request(movieURL, headers=self.hdr))
             moviePageSoup = BeautifulSoup(moviePageHtml, 'html.parser')
             movie['name'] = str(moviePageSoup.find('h2',{'class':'movie-original-title'}).string)
             movie['duration'] = str(moviePageSoup.find('span',{'class':'running_time'}).string)
